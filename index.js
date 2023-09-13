26ca353582fbbf8512c0253caf12fc715e595c79
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-09-14 00:11:31 +0200

    added modal for project

--- index.js
+++ index.js
@@ -106,17 +106,17 @@
 
 //-------------------------task 8
 
-function chunkArrayInGroups(arr, size) {
-    let newArr = [];
-    for (let i = 0; i < arr.length; i+=size) {
-            newArr.push(arr.slice(i, i + size));
+// function chunkArrayInGroups(arr, size) {
+//     let newArr = [];
+//     for (let i = 0; i < arr.length; i+=size) {
+//             newArr.push(arr.slice(i, i + size));
     
-    }
+//     }
 
-  return newArr;
-}
+//   return newArr;
+// }
 
-console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+// console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
 
 
 
