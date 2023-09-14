aaeb7f0e6e2f67256be28b6a5e8a52cb0d4caee8
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-09-14 10:28:14 +0200

    add event listner

--- index.js
+++ index.js
@@ -123,9 +123,64 @@
 
 
 
+const modalCloseBtnOrder = document.querySelector('.modal-close-btn-order');
+const backdropEl = document.querySelector('.backdrop');
+const formOrder = document.querySelector('.modal-form-order');
+
+
+// клік на btnClose
+modalCloseBtnOrder.addEventListener('click', onBtnClose);
+// клік на backdropEl
+backdropEl.addEventListener('click', onclickBackdrop);
+// клік на Escape
+document.addEventListener("keydown", onClickEscape);
+// клік на Send
+formOrder.addEventListener('submit', onClickSend);
+
+
+function onBtnClose(e) {
+ backdropEl.classList.add('is-hidden')}
+
+function onclickBackdrop() {
+    backdropEl.classList.add('is-hidden')
+}
+
+function onClickEscape(e) {
+  if (e.code === "Escape") {
+
+    console.log("Closing window...");
+    document.removeEventListener("keydown", onClickEscape) } //  знімаємо слухача на Escape
+
+  
+}
 
 
 
 
 
 
+
+function onClickSend(evt) {
+
+  evt.preventDefault();  
+
+    const { user_name, user_phone, user_mail_modal, modal_comment  } = evt.target.elements;
+ 
+    // перевірка чи заповнені поля вводу
+    if (user_phone === "" || user_mail_modal.value === "" || user_name.value === '' || modal_comment.value ==='') {
+        alert('All fields must be filled')
+    }
+    else {
+      console.log({
+        'Name': user_name.value,
+        'Phone number': user_phone.value,
+        'Email': user_mail_modal.value,
+        'Comment': modal_comment.value
+      })
+      
+    form.reset()   //очищаємо поле
+    }
+         
+    
+    
+}
